d4d42e86fc517a97a0d1791e2a3fa5910cf741f3
Angel <ang4732@gmail.com>
2021-03-12 00:17:22 -0500

    script to production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
+const URL_BASE="https://myapiangel.herokuapp.com/api/"
+
 export const environment = {
-  production: true
+  production: true,
+  uriCategoria: URL_BASE +"categories",
+  uriProductos: URL_BASE +"products",
+  uriProductoCategoria: URL_BASE +"products/search/findByCategoryId?id=",
+  uriCheckout: URL_BASE +"checkout/purchase"
 };
